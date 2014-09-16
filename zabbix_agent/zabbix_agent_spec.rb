b3347a12438471978eb83d203aa7316099665464
knakayama <knakayama.sh@gmail.com>
2014-09-16 22:06:22 +0900

    add zabbix-agent spec

---
+++ zabbix_agent/zabbix_agent_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe package('zabbix-agent') do
+    it { should be_installed }
+end
+
+describe service('zabbix-agent') do
+    it { should be_enabled }
+    it { should be_running }
+end
+
+describe port(10050) do
+    it { should be_listening.with('tcp') }
+end
+
+describe file('/etc/zabbix/zabbix_agend.conf') do
+    it { should be_file }
+    it { should be_owned_by root }
+    it { should be_grouped_into root }
+    it { should be_mode 644 }
+    its(:content) { should match /127\.0\.0\.1|172\.31\.21\.235/ }
+end
+
